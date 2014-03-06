8a85662b162d54785bdd4c2a4ba64c4677531580
kkimeng <kky@macalester.edu>
2014-03-06 16:42:22 -0600

    in class

--- src/edu/macalester/comp124/wordcounter/AllWordsCounter.java
+++ src/edu/macalester/comp124/wordcounter/AllWordsCounter.java
@@ -13,12 +13,18 @@ public class AllWordsCounter {
     public static final int MAX_WORDS = 10000;
 
 	// TODO: initialize instance variable to hold MAX_WORDS objects
-    SingleWordCounter counters[];
+    SingleWordCounter counters[] = new SingleWordCounter[MAX_WORDS];
 
     public int getNumWords() {
         // TODO: count the number of distinct words,
         // ie. the number of non-null counter objects.
-        return -1;
+        int count = 0;
+        for (SingleWordCounter s : counters) {
+            if (s != null) {
+            count++;
+            }
+        }
+        return count;
     }
 	
 	/**
@@ -30,12 +36,13 @@ public class AllWordsCounter {
 	public void count(String word) {
         int n = getNumWords();
         for (int i = 0; i < n; i++) {
-            // If you find the word increment the count and return
+            if (counters[i].wordMatches(word)) {
+                counters[i].incrementCount();
+                return;
+            }
         }
-
-        // You didn't find the word. Add a new word counter to the array.
-        // Don't forget to increment the word's count to get it to 1!
-	}
+        counters[n] = new SingleWordCounter(word);
+    }
 	
 	/**
 	 * Return the count for the particular word.  Remember that the
@@ -46,7 +53,13 @@ public class AllWordsCounter {
 	public int getCount(String word) {
         // TODO: pattern this after the count() function.
         // Make sure to return 0 for words you haven't seen before.
-        return -1;
+        int n = getNumWords();
+        for (int i = 0; i < n; i++) {
+            if (counters[i].wordMatches(word)) {
+                return counters[i].getCount();
+            }
+        }
+        return 0;
 	}
 	
 	/**
@@ -56,8 +69,10 @@ public class AllWordsCounter {
 	public String []  getAllWords() {
         // part one: create an array of strings of size equal to the number of words
         int n = getNumWords();
-        String words[] = null;  // FIXME
-
+        String words[] = new String[n];  // FIXME
+        for (int i = 0; i < n; i++) {
+            words[i] = counters[i].getWord();
+        }
         // part two: fill the array of strings using a loop
 
         return words;
--- src/edu/macalester/comp124/wordcounter/SingleWordCounter.java
+++ src/edu/macalester/comp124/wordcounter/SingleWordCounter.java
@@ -15,7 +15,7 @@ public class SingleWordCounter {
      */
     public SingleWordCounter(String word) {
         this.word = word;
-        this.count = 0;
+        this.count = 1;
     }
 
     /**
